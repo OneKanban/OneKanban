524ef5e8031048a88e5f9abcb90b2f5fcfa717e3
Oliver Oyston <oliveroyston@hotmail.com>
2025-07-03 23:37:15 +0100

    Initial customisations

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,14 +6,14 @@ import starlight from '@astrojs/starlight';
 export default defineConfig({
 	integrations: [
 		starlight({
-			title: 'My Docs',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
+			title: 'One Kanban',
+			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/OneKanban/OneKanban' }],
 			sidebar: [
 				{
 					label: 'Guides',
 					items: [
 						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
+						{ label: 'Kanban', slug: 'guides/kanban' },
 					],
 				},
 				{
